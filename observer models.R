8e14331289fc22d6ee9de393f6f4eac80ff156cf
Naoise <s2848034@ed.ac.uk>
2026-06-30 17:30:46 +0100

    add dist component to models. rename ground_truth

--- observer models.R
+++ observer models.R
@@ -1,13 +1,13 @@
 model_one_observer_hn <- function(
     dd, 
-    ground_truth, # for the mesh, buffered transects and so on
+    construction_info, # for the mesh, buffered transects and so on
     mtrn_prior =matern_prior,
     half_width=8
 ){
 
   #assuming hn, log_hn, dist_to_nearest_line_transect are defined globally
   
-  mesh1 <- ground_truth$the_mesh
+  mesh1 <- construction_info$the_mesh
   
   #save compute and space if a previous model already made it
   matern_prior = mtrn_prior
@@ -21,12 +21,18 @@ model_one_observer_hn <- function(
     sigma(1,
            prec.linear = 1,
            marginal = bm_marginal(qexp, pexp, dexp, rate = 1/half_width) #need to think more about this - effect on detection at end of halfwidth
-    ) +spde(main=geometry, model = matern_prior)
+    ) +
+    spde(main=geometry, model = matern_prior) +
+    dist_to_transect(
+      dist_to_nearest_line_transect(geometry, construction_info$line_transects),
+      model = "const"
+    )
+  
   
   
   form <- geometry  ~ Intercept  +
     log_hn(
-      dist_to_nearest_line_transect(geometry, ground_truth$line_transects),
+      dist_to_transect,
       sigma
     ) +log(2)+ spde
   
@@ -37,7 +43,7 @@ model_one_observer_hn <- function(
     data =  dd,
     domain = list(geometry=fm_subdivide(mesh1, 3) ),
     # the observed regions are line transects expanded outwards by the transect widths, right?
-    samplers = ground_truth$buffered_transects,
+    samplers = construction_info$buffered_transects,
     options = list(bru_verbose= 1,
                    # verbose = 4,
                    bru_initial = list(sigma = half_width/4) # need to review this 
@@ -50,11 +56,11 @@ model_one_observer_hn <- function(
 
 model_two_observers_hn <- function(
   dd, 
-  ground_truth, # for the mesh, buffered transects and other info
+  construction_info, # for the mesh, buffered transects and other info
   mtrn_prior = matern_prior, # save memory
   half_width = 8
 ){
-  mesh1 <- ground_truth$the_mesh
+  mesh1 <- construction_info$the_mesh
   # the space is now the 2D map and detection state
   the_whole_domain = list(geometry=fm_subdivide(mesh1, 3), detected=1:3)
   
@@ -76,12 +82,16 @@ model_two_observers_hn <- function(
            prec.linear = 1,
            marginal = bm_marginal(qexp, pexp, dexp, rate = 1/8)
     )+
-    spde(main=geometry, model = matern_prior)
+    spde(main=geometry, model = matern_prior) +
+    dist_to_transect(
+      dist_to_nearest_line_transect(geometry, construction_info$line_transects),
+      model = "const"
+    )
   
   
   form <- geometry + detected  ~ Intercept  +
     log_g_2observer(
-      dist_to_nearest_line_transect(geometry, ground_truth$line_transects),
+      dist_to_transect,
       detected,
       sigmaA, sigmaB
     ) +log(2)+ spde
@@ -94,7 +104,7 @@ model_two_observers_hn <- function(
     domain = the_whole_domain,
     # the observed regions are line transects expanded outwards by the transect widths, right?
     # not sure what'll happen with the detected states
-    samplers = ground_truth$buffered_transects,
+    samplers = construction_info$buffered_transects,
     options = list(bru_verbose= 1,
                    # verbose = 4,
                    bru_initial = list(sigmaA = half_width/4, sigmaB = half_width/4) # need to review this
