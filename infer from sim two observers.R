5e3e27d0df38abea8905a237a7b6542325a77f60
Naoise <s2848034@ed.ac.uk>
2026-07-04 18:19:29 +0100

    remove old code. add futher scoring rules. add plots of score differences

--- infer from sim two observers.R
+++ infer from sim two observers.R
@@ -54,18 +54,6 @@ detect_func_2observer_sigma <- function(distance, sigmaA, sigmaB){
 
 ######## functions for assessing model fit 
 
-get_spatial_param_width <- function(mdl, param, correct){
-  # checks if the model's 95% credible interval contains the true value of a hyperparam
-  # if so it returns 1 and the interval width
-  cred_int <- mdl$summary.hyperpar[param, c("0.025quant", "0.975quant")]
-  cred_int <- unlist(cred_int)
-  
-  if ( cred_int[1] <= correct & correct <= cred_int[2] ){
-    c(1, diff(cred_int) )
-  } else {
-    c(0, diff(cred_int) )
-  }
-}
 
 dawid_sebastiani_score <- function(post_pred, true_value){
   E <- post_pred$mean
@@ -74,52 +62,73 @@ dawid_sebastiani_score <- function(post_pred, true_value){
 }
 
 
-compare_merged_vs_two_observer_models <- function(
-    merged, two_obs,
-    true_sigmaA, true_sigmaB, dist_res,
-    do_plot = F,
-    true_spatial_range=true_range, true_spatial_stdv = true_sigma_grf
-    ){
+get_scoring_differences <- function(
+    sim_info, fit_merged_observers, fit_two_observers,
+    true_detect_prob = detect_func_2observer_sigma(dists, true_sigmaA, true_sigmaB),
+    dists = seq(0,8, length.out=1000)
+){
+  #for brevity
+  loglambda <- sim_info$log_lambda
   
-  output <- matrix(NA, 2, ncol=1+4+1)
+  #both models predict log lambda, lambda, and the probability of (any) detection
+  # squeezing these all into one predict call so it'll hopefully be faster
+  merged_observer_pred <- predict(
+    fit_merged_observers, 
+    newdata = list(
+      geometry=sim_info$interior_vertices$geometry,
+      dists=dists
+    ),
+    formula = ~ {
+      list(
+        lambda = exp(Intercept + spde),
+        log_lambda = Intercept + spde,
+        detection = hn(dists, sigma)
+      )
+    },
+    n.samples=2000,
+  )
+  # different detection prob function for two observer model
+  two_observers_pred <- predict(
+    fit_two_observers, 
+    newdata = list(
+      geometry=sim_info$interior_vertices$geometry,
+      dists=dists
+    ),
+    formula = ~ {
+      list(
+        lambda = exp(Intercept + spde),
+        log_lambda = Intercept + spde,
+        detection = detect_func_2observer_sigma(dists, sigmaA, sigmaB)
+      )
+    },
+    n.samples=2000
+  )
   
+  # DS scores on log lambda at vertices
+  merged_observers_DS <- dawid_sebastiani_score(merged_observer_pred$log_lambda, loglambda)
+  two_observers_DS <- dawid_sebastiani_score(two_observers_pred$log_lambda, loglambda)
+  # integrated DS score difference over the study region
+  integrated_DS_difference <- sum(sim_info$interior_ips$weight * (merged_observers_DS - two_observers_DS) )
   
-  true_detect <- detect_func_2observer_sigma(dist_res, true_sigmaA, true_sigmaB)
+  # MSE scores on the posterior mean for log lambda
+  merged_observers_MSE <- mean(merged_observer_pred$log_lambda$mean - loglambda)
+  two_observers_MSE <- mean(two_observers_pred$log_lambda$mean - loglambda)
   
-  output[1:2, i<-1] <- c("merged", "two obs")
+  # MAE scores on the posterior median for lambda
+  merged_observers_MAE <- mean(abs( merged_observer_pred$lambda$median - exp(loglambda) )) 
+  two_observers_MAE <- mean(abs( two_observers_pred$lambda$median - exp(loglambda) )) 
   
-  output[1, 2:3] <- get_spatial_param_width(merged, "Range", true_spatial_range)
-  output[1, 4:5] <- get_spatial_param_width(merged, "Stdev", true_spatial_stdv)
+  # MAE errors on the posterior mean detection probability
+  merged_observers_detect_MAE <- mean(abs( merged_observer_pred$detection$mean - true_detect_prob ))
+  two_observers_detection_MAE <- mean(abs( two_observers_pred$detection$mean - true_detect_prob ))
   
-  pred_detect_merged_obs <- predict(merged, formula = ~ hn(dist_res, sigma))
-  output[1, 6] <- mean(abs( pred_detect_merged_obs$mean - true_detect ))
-  
-  
-  output[2, 2:3] <- get_spatial_param_width(two_obs, "Range", true_spatial_range)
-  output[2, 4:5] <- get_spatial_param_width(two_obs, "Stdev", true_spatial_stdv)
-  
-  pred_detect_two_obs <- predict(two_obs, formula = ~ detect_func_2observer_sigma(dist_res, sigmaA, sigmaB) )
-  output[2, 6] <- mean(abs( pred_detect_two_obs$mean - true_detect ))
-  
-  if( do_plot){
-    res <- rbind(
-      cbind(pred_detect_two_obs, model="two obs"),
-      cbind(pred_detect_merged_obs, model="merged") 
-    )
-    res$distance <- rep(dist_res, 2)
-    res$Truth <- rep(true_detect, 2)
-    
-    g <- ggplot(res) +
-      geom_ribbon(aes(x=distance, ymin=q0.025, max=q0.975, fill=model),  alpha=.3 ) +
-      geom_line(aes(x=distance, y=mean,color=model), linewidth=1.5) +
-      geom_line(aes(distance, Truth), linewidth=1.5) +
-      labs(y="detection probability", title = "Estimates of the detection function \t Black = Truth") +
-      ylim(0,1)
-    
-    print(g)
-  }
-  
-  output
+  #report the differences in scores relative to a single merged observer model
+  list(
+    DS = integrated_DS_difference,
+    MSE =  merged_observers_MSE - two_observers_MSE,
+    MAE = merged_observers_MAE - two_observers_MAE,
+    MAE_detection = merged_observers_detect_MAE - two_observers_detection_MAE
+  )
 }
 
 ########### simulate the ground truth
@@ -149,117 +158,87 @@ sim_info <- simulate_lcgp_distance_thinning(
   true_rho = true_range, true_sigma_GRF=true_sigma_grf
 )
 
-#the sampled points are in dd
-dd <- sim_info$samples_df
+#the sampled points are in observed_points
+observed_points <- sim_info$samples_df
 
 # Model what A and B saw as a combined observer
 catt("fitting merged")
-start <- Sys.time()
-fit_merged_observers <- model_one_observer_hn(dd, sim_info, matern_prior)
-end <- Sys.time()
-catt(end-start)
+start <- proc.time()
+fit_merged_observers <- model_one_observer_hn(observed_points, sim_info, matern_prior)
+end <- proc.time()
+print((end-start)[3])
 
 # Model what A and B saw as a two observer likelihood
 catt("fitting two")
-start <- Sys.time()
-fit_two_observers <- model_two_observers_hn(dd, sim_info, matern_prior)
-end <- Sys.time()
-catt(end-start)
+start <- proc.time()
+fit_two_observers <- model_two_observers_hn(observed_points, sim_info, matern_prior)
+end <- proc.time()
+print((end-start)[3])
+
+get_scoring_differences(sim_info, fit_merged_observers, fit_two_observers)
 
 
-get_DS_score_difference <- function(
-    sim_info, fit_merged_observers, fit_two_observers
-    ){
-  
-  #both models predict log lambda
-  merged_observers_pred <- predict(
-    fit_merged_observers, sim_info$interior_vertices,
-    formula = ~ {
-      list(
-        lambda=exp(Intercept + spde),
-        log_lambda = Intercept + spde
-      )
-    },
-    n.samples=2000
+
+set.seed(1234)
+nsims <- 20
+results <- NULL
+
+for (i in 1:nsims){
+  catt("Simulation", i, "of", nsims)
+
+  # simulate a ground truth
+  sim_info <- simulate_lcgp_distance_thinning(
+    detect_func = hn,
+    detect_func_paramA = true_sigmaA,
+    detect_func_paramB = true_sigmaB,
+    true_beta0 = -5,
+    true_rho = true_range, true_sigma_GRF=true_sigma_grf
   )
-  two_observers_pred <- predict(
-    fit_two_observers, sim_info$interior_vertices,
-    formula = ~ {
-      list(
-        lambda=exp(Intercept + spde),
-        log_lambda = Intercept + spde
-      )
-    },
-    n.samples=2000
+
+  #the sampled points are in observed_points
+  observed_points <- sim_info$samples_df
+
+  # Model what A and B saw as a combined observer
+  catt("fitting merged")
+  start <- proc.time()
+  fit_merged_observers <- model_one_observer_hn(observed_points, sim_info, matern_prior)
+  end <- proc.time()
+  print((end-start)[3])
+
+  # Model what A and B saw as a two observer likelihood
+  catt("fitting two")
+  start <- proc.time()
+  fit_two_observers <- model_two_observers_hn(observed_points, sim_info, matern_prior)
+  end <- proc.time()
+  print((end-start)[3])
+
+  catt("comparing the two")
+  start <- proc.time()
+  results <- rbind(
+    results, get_scoring_differences(sim_info, fit_merged_observers, fit_two_observers)
   )
-  # DS score at vertices
-  merged_observers_DS <- dawid_sebastiani_score(merged_observers_pred$log_lambda, sim_info$log_lambda)
-  two_observers_DS <- dawid_sebastiani_score(two_observers_pred$log_lambda, sim_info$log_lambda)
-  
-  length(merged_observers_DS); length(two_observers_DS);length(sim_info$log_lambda)
-  # get the difference in DS scores relative to a single merged observer model
-  DS_score_difference <- merged_observers_DS - two_observers_DS
-  
-  # integrated DS score difference over the study region
-  integrated_DS_difference <- sum(sim_info$interior_ips$weight * DS_score_difference)
-  
-  integrated_DS_difference
+  end <- proc.time()
+  print((end-start)[3])
 }
+(results <- as.data.frame( apply(results, 2, as.numeric) ) )
+
+# save the results
+saveRDS(results, file="sat-4-07-score-results.rda")
 
 
-get_DS_score_difference(sim_info, fit_merged_observers, fit_two_observers)
+# plot the difference in scores, a difference of zero implies the models perform the same wrt to that score
+# all scores are negatively orientated so a positive difference means the simpler merged observer model
+# performed better
+# the Dawid-Sebastiani score is on a very large scale so im omitting the zero baseline
 
-# set.seed(1234)
-# nsims <- 10
-# results <- matrix(NA, nrow=2*nsims, ncol=6)
-# colnames(results) <- c("model", "range_correct", "range_CI_width", "stdv_correct", "stdv_CI_width", "MAE_detection")
-# 
-# for (i in 1:nsims){
-#   catt("Simulation", i, "of", nsims)
-#   
-#   # simulate a ground truth
-#   sim_info <- simulate_lcgp_distance_thinning(
-#     detect_func = hn,
-#     detect_func_paramA = true_sigmaA,
-#     detect_func_paramB = true_sigmaB,
-#     true_beta0 = -5,
-#     true_rho = true_range, true_sigma_GRF=true_sigma_grf
-#   )
-#   
-#   #the sampled points are in dd
-#   dd <- sim_info$samples_df
-#   
-#   # Model what A and B saw as a combined observer
-#   catt("fitting merged")
-#   start <- Sys.time()
-#   fit_merged_observers <- model_one_observer_hn(dd, sim_info, matern_prior)
-#   end <- Sys.time()
-#   catt(end-start)
-#   
-#   # Model what A and B saw as a two observer likelihood
-#   catt("fitting two")
-#   start <- Sys.time()
-#   fit_two_observers <- model_two_observers_hn(dd, sim_info, matern_prior)
-#   end <- Sys.time()
-#   catt(end-start)
-#   
-#   catt("comparing the two")
-#   results[2*i-1:0,] <- compare_merged_vs_two_observer_models(
-#     fit_merged_observers, fit_two_observers,
-#     true_sigmaA, true_sigmaB, dists,
-#     do_plot = T
-#   )
-# }
-# results
-# 
-# 
-# results <- as.data.frame(results)
-# # the model column is a character so so did everything else in the matrix  
-# results[-1] <- lapply(results[-1], as.numeric)
-# 
-# 
-# 
-# save(results, file="-sims.rda")
+dsp <- ggplot(results) + geom_boxplot(aes(y=DS))
+maep <- ggplot(results) + geom_boxplot(aes(y=MAE)) + expand_limits(y=0)
+msep <- ggplot(results) + geom_boxplot(aes(y=MSE)) +  expand_limits(y=0)
+maedetectp <- ggplot(results) + geom_boxplot(aes(y=MAE_detection)) + expand_limits(y=0)
+
+(dsp + maep) / ( msep + maedetectp)
+
+
 
 
 
