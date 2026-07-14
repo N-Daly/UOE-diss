3defbed3291953dd13996eeda1446c0cc1dca586
Naoise <s2848034@ed.ac.uk>
2026-07-14 19:33:51 +0100

    add final spatially varying mesh creation function

--- observer models.R
+++ observer models.R
@@ -1,3 +1,40 @@
+make_spatially_varying_mesh <- function(num_vertices, sim_info){
+  
+  # this determines the target max edge length from each initially seeded vertex
+  qual_loc <- function(locs){
+    dist <- dist_to_nearest_line_transect(locs, sim_info$line_transects)
+    #round the distance so that there are less unique edge lengths initially
+    pmin(20, pmax(0.5, floor(dist) ))
+  }
+  
+  # a grid of points across the study region, 40km apart
+  seed_points <- fm_hexagon_lattice(sim_info$boundary, edge_len = 40)
+  
+  # also include points on the line transects themselves
+  # so that in the following delauney triangulation the transects will be better captured
+  pts_near_line <- st_cast(sim_info$line_transects$geometry, "POINT")
+  seed_points <- c(seed_points, pts_near_line)
+  
+  
+  # make a mesh with:
+  # an extension outside the study region, around 10% farther
+  # no triangle angles smaller than 23 degrees
+  # there is some user set number of triangles, once reached the algorithm starts resizing edge lengths
+  mesh <- fm_rcdt_2d(
+    loc = seed_points
+    , extend = list(offset=-.1),
+    refine = list(
+      min.angle = 23,
+      max.n = num_vertices*1000
+    ),
+    quality.spec = list(
+      loc = qual_loc(seed_points)
+    ),
+    crs=sim_info$the_mesh$crs
+  )
+  mesh
+}
+
 model_one_observer_hn <- function(
     dd, 
     construction_info, # for the mesh, buffered transects and so on
