c2dc826160c6ec2875fedf0dbadf4fdb85e05e92
Naoise <s2848034@ed.ac.uk>
2026-07-17 16:36:35 +0100

    reimplement mesh creation usimg line transects

--- mesh resolution from dist discrepancies.R
+++ mesh resolution from dist discrepancies.R
@@ -30,7 +30,30 @@ sim_info <- simulate_lcgp_distance_thinning(
 pts <- sim_info$samples_df
 actual_distance <- pts$distance
 
-# reconstruct the points' perpendicular distance using the mesh 
+get_nonoverlapping_samplers <- function(ltransects=mexdolphin_sf$samplers){
+
+  nonoverlapping <- c(1)
+  b_trans <- st_buffer(ltransects, dist = 2, endCapStyle = "ROUND")
+  
+  for (i in 2:nrow(ltransects)){
+    l <- b_trans[i,]
+    
+    overlaps <- st_intersects(
+      b_trans[nonoverlapping,],
+      b_trans[i, ] ,
+      sparse = F
+    )
+    
+    if ( !any(overlaps) ){
+      nonoverlapping <- rbind(nonoverlapping, i)
+    }
+  }
+  ltransects[nonoverlapping,]
+}
+
+nonoverlapping <- get_nonoverlapping_samplers()
+
+# reconstruct the points' perpendicular distance using the mesh
 eval_mesh <- function(a_mesh){
 
   field <- dist_to_nearest_line_transect(fm_vertices(a_mesh), sim_info$line_transects)
@@ -38,9 +61,11 @@ eval_mesh <- function(a_mesh){
    approximated_distance <- fm_evaluate(
     a_mesh, field = field, loc= pts$geometry
   )
-  
+
   approximated_distance
 }
+
+# reconstruct the point's
 
 # plot the reconstructed distances against actual ones
 # and get the mean absolute deviation, as a percentage of the true value
@@ -121,22 +146,22 @@ repeated_model_fitting <- function(..., nreps=5){
 ################ the actual function ############
 
 make_spatially_varying_mesh <- function(num_vertices){
-  
+
   # this determines the target max edge length from each initially seeded vertex
   qual_loc <- function(locs){
     dist <- dist_to_nearest_line_transect(locs, sim_info$line_transects)
     #round the distance so that there are less unique edge lengths initially
     pmin(20, pmax(0.5, floor(dist) ))
   }
-  
+
   # a grid of points across the study region, 40km apart
   seed_points <- fm_hexagon_lattice(sim_info$boundary, edge_len = 40)
-  
+
   # also include points on the line transects themselves
   # so that in the following delauney triangulation the transects will be better captured
   pts_near_line <- st_cast(sim_info$line_transects$geometry, "POINT")
   seed_points <- c(seed_points, pts_near_line)
-  
+
 
   # make a mesh with:
   # an extension outside the study region, around 10% farther
@@ -157,18 +182,68 @@ make_spatially_varying_mesh <- function(num_vertices){
   mesh
 }
 
+make_spatially_varying_mesh2 <- function(param, transects){
+
+  hex_points <- fm_hexagon_lattice(mexdolphin_sf$ppoly, edge_len = 20)
+  transect_points <- st_cast(nonoverlapping$geometry, "POINT")
+  seed_points <- c(transect_points, hex_points)
+  
+  mesh1 <- fm_rcdt_2d(
+    loc = seed_points,
+    boundary = mexdolphin_sf$ppoly,
+    refine=F,
+    extend = list(offset = -0.1),
+    crs = mexdolphin_sf$mesh$crs
+  )
+
+  ## Save the resulting boundary
+  boundary1 <- fm_segm(mesh1, boundary = TRUE)
+  interior1 <- fm_segm(mesh1, boundary = FALSE)
+  
+  ## Triangulate inner domain
+  mesh2 <- fm_rcdt_2d(
+    loc = seed_points,
+    interior = fm_segm( interior1, nonoverlapping$geometry, is.bnd=F),
+    boundary = boundary1,
+    refine = list(
+      min.angle = 27
+    ),
+    crs = mesh1$crs
+  )
+  
+
+  boundary2 <- fm_segm(mesh2, boundary = TRUE)
+  interior2 <- fm_segm(mesh2, boundary = FALSE)
+  
+  mesh3 <- fm_rcdt_2d(
+    loc = rbind(seed_points, fm_vertices(mesh2)),
+    interior = fm_segm(boundary2, interior2, is.bnd=F),
+    refine = list(
+      min.angle = 27
+    ),
+    crs = mesh1$crs
+  )
+  
+  if (param == 0){
+    mesh3
+  } else{
+    fm_subdivide(mesh3, param)
+  }
+}
 
 #c(100, 50, 30, 25, 20) 
 fit_time <- NULL
 param <- NULL
 error <- NULL
 #seq(10,60, by=5)
-for (i in  seq(10, 100, by = 10)){
+for (i in 0:3){
   cat("\n param =", i, "\n")
+  
+  rm(m, ips, some_model_fit); gc()
   start <- proc.time()
   cat("making mesh and ips \n")
   
-  m <- make_spatially_varying_mesh(i)
+  m <- make_spatially_varying_mesh2(i, nonoverlapping)
   approx_dist <- eval_mesh(m)
 
   ips <- fm_int(
@@ -183,7 +258,7 @@ for (i in  seq(10, 100, by = 10)){
   mpe <- plot_results(approx_dist)
   mtext(paste("param=",i, "# vertices =", nrow(fm_vertices(m)) ))
   
-  gc()
+  
   #fit a simple model with spatial random effect and hn detection function
   start <- proc.time()
   some_model_fit <- fit_model_with_it(m, ips)
@@ -206,9 +281,9 @@ for (i in  seq(10, 100, by = 10)){
   error <- c(error, mpe)
   
   # quick save results in case R conks out on a big mesh
-  save(fit_time, param, error, file="quicksave.rda")
+  # save(fit_time, param, error, file="nospec_then_subdivide_quicksave.rda")
 }
-load("quicksave.rda")
+# load("27quicksave.rda")
 
 # i dont see a way of doing this in ggplot, not do i see a potential benefit
 plot(
@@ -221,7 +296,7 @@ plot(
   ylab = "fitting time in seconds",
   main = "Trade off between computational time and mesh quality"
 )
-mtext("Number indicates the number of vertices in each mesh, in 1000s")
+mtext("Number indicates the number of subdivisions performed in each mesh")
 text(
   error, 
   fit_time,
@@ -235,5 +310,14 @@ text(
 #   gg(sim_info$buffered_transects, color="brown", alpha=.2)+
 #   ggspatial::annotation_scale(location="tr")
 
+m <- make_spatially_varying_mesh2(2, nonoverlapping)
+approx_dist <- eval_mesh(m)
+
+ips <- fm_int(
+  m, samplers = sim_info$buffered_transects
+)
+ips$distance <- dist_to_nearest_line_transect(ips$geometry, sim_info$line_transects)
+saveRDS(ips, "ips_interiorTransects_2subdivisions.rda")
+getwd()
 
 
