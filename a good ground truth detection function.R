8ff5ed0e0afcf6f64d621fcbfd5354443fed02cc
Naoise <s2848034@ed.ac.uk>
2026-08-01 16:35:42 +0100

    add ground truth detection plot

--- a good ground truth detection function.R
+++ a good ground truth detection function.R
@@ -1,4 +1,6 @@
 source("function Definitions.R")
+library(reshape2)
+library(ggplot2)
 
 dd <- seq(0, 8, length.out=1000)
 
@@ -6,13 +8,45 @@ pa <- hr(dd, 1.75, 1)
 pb <- hr(dd, 2, 4)
 pdual <- 1 - (1-pa)*(1-pb)
 
+par(cex=1)
+
 plot(
   dd,
   pdual,
   ylim = 0:1,
-  ylab = "",
-  xlab = "distance",
-  lwd = 1
+  ylab = "Detection probability",
+  xlab = "Distance from observers, km",
+  lwd = 2,
+  type="l",
+  main = "True probability of detection within transect segment"
 )
-lines(dd, pa, col = "red")
-lines(dd, pb, col = "blue")
+lines(dd, pa, col = "red", lwd = 2)
+lines(dd, pb, col = "blue", lwd = 2)
+
+legend(
+  "topright",
+  legend = c("Observer A and/or B", "Observer A", "Observer B"),
+  col = c("black", "red", "blue"),
+  lwd = "3"
+)
+
+# https://stackoverflow.com/questions/23635662/editing-legend-text-labels-in-ggplot
+df <- data.frame(distance=dd, pa=pa, pb=pb, pdual = pdual)
+dfm <- melt(df, id= "distance")
+
+ggplot(dfm, aes(x=distance, y = value, color = variable)) +
+  geom_line(lwd=2) +
+  scale_color_manual(
+    breaks = c("pdual", "pa", "pb"),
+    labels = c("Observer A and/or B", "Observer A", "Observer B"),
+    values = c("black", "red", "blue")
+  ) +
+  ylab("Detection probability") +
+  xlab("Distance from observers, km") +
+  labs(title = "True probability of detection within transect segment") + 
+  theme_bw() +
+  guides() +
+  theme(
+    legend.background = element_rect(fill = "white"),
+    legend.position.inside  =c(1,1)
+    )
