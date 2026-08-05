7164fce184941eb973be153d17d7579659f72d1b
Naoise <s2848034@ed.ac.uk>
2026-08-05 19:45:42 +0100

    re-examine spline priors with both boundary conditions

--- messing with spline priors.R
+++ messing with spline priors.R
@@ -1,4 +1,4 @@
-# rm(list=ls());while(dev.cur()>1){dev.off()};old_par<- par(no.readonly = T, pch=19)
+rm(list=ls());while(dev.cur()>1){dev.off()};old_par<- par(no.readonly = T, pch=19)
 
 library(INLA)
 library(inlabru)
@@ -6,24 +6,19 @@ library(sf)
 library(fmesher)
 library(ggplot2)
 
+source("function Definitions.R")
+
 dists_grid <- seq(0,8, length.out=1000)
 
-spline_to_detect_func <- function(x){
-  # converts the value of the spline at a point to the corresponding value
-  # of the detection function at that point
-  exp(-x)
-}
-
-
 mesh <- fm_mesh_1d(
-  loc = 0:8,
+  loc =  c(0,2,4,6,8),
   boundary = c("dirichlet", "free"),
   degree = 2
 )
 projector <- fm_evaluator(mesh, loc=dists_grid)
 
-# ggplot() + geom_fm(data= mesh)
 
+#not sure what good this is without accounting for the neumann condition
 plot_the_variance <- function(
     the_mesh=mesh, distance=dists_grid,
     alpha, rho, sigma
@@ -35,7 +30,7 @@ plot_the_variance <- function(
   
   Sigma <- diag( fm_covariance(Q, basis) )
   
-  title = "Marginal variance of the Spline representation"
+  title = "Marginal variance of the Spline"
   subtitle = paste(
     " alpha =",alpha,
    "rho =", rho,
@@ -43,72 +38,65 @@ plot_the_variance <- function(
   )
   plot(
     distance, Sigma,
-    ylim=0:1,
+    # ylim=0:1,
     type="l",
     main = title
   )
   mtext(subtitle)
 }
+plot_the_variance(alpha=2,rho=1,sigma=1)
 
-sample_a_detect_func_prior <- function(alpha, rho, sigma, evaluator=projector, the_mesh=mesh){
-  
-  prior_weights <- fm_matern_sample(
-    the_mesh, 
-    alpha=alpha, rho = rho, sigma = sigma,
-  
-  )
-  spline_values <- fm_evaluate(proj=projector, field=prior_weights)
-  spline_to_detect_func(spline_values)
-  # spline_values
-}
 
-# plot(dists_grid, sample_a_detect_func_prior(2,1,1))
-
-sample_detect_funcs_and_plot <- function(
-    nreps=100, distances=dists_grid,
-    alpha, rho, sigma
+sample_many_detect_priors <- function(
+    rho, sigma,
+    the_mesh = mesh, proj = projector, distances = dists_grid,
+    alpha = 2, nsims = 100
   ){
-  many_dfs <- lapply(
-    1:nreps,
-    function(i){
-      data.frame(
-        x = distances,
-        y = sample_a_detect_func_prior(alpha, rho, sigma),
-        iter=i
-      )
-    }
-  )
-  samples <- do.call(rbind, many_dfs)
   
-  main = paste(
-    nreps, 
-    "realisations of Matern with alpha =",
-    alpha,
-    "rho =", rho,
-    "sigma = ", sigma
+  Q <- fm_matern_precision(the_mesh, alpha, rho, sigma)
+  
+  dof <- fm_dof(the_mesh)
+  
+  prior_weights <- fm_sample(
+    n=nsims,
+    Q=Q,
+    constr = list(
+      A = matrix(c(1, rep(0, dof-1) ), 1, dof),
+      e = matrix(0,1,1)
+    )
   )
-  g<- ggplot(samples, aes(x,y, group=iter))  + 
-    geom_line(alpha=.4) +
-    expand_limits(y=0:1) + ylim(c(0, 2)) +
-    labs(title = main)
-  print(g)
-  #samples
+  
+  spline_values <- fm_evaluate(proj=proj, field=prior_weights)
+  
+  detect_values <- spline_detect_func(spline_values)
+
+  df <- data.frame(
+    prob = c(detect_values),
+    dist = rep(distances, times = ncol(detect_values)),
+    sim = rep(1:nsims, each = nrow(detect_values))
+  )
+  df
 }
 
-alpha=2; rho=2; sigma=1.5
+rho <- 3; sigma <- 0.5
+ss <- sample_many_detect_priors(rho=rho, sigma = sigma, nsims=200)
 
-
-set.seed(123)
-sample_detect_funcs_and_plot(
-  nreps=100,
-  alpha=alpha, rho=rho, sigma=sigma
+title <- paste(
+  "Realisations of spline detect function with rho =",
+  rho,
+  "and sigma =",
+  sigma
 )
-# 
-# 
-# plot_the_variance(
-#   mesh,
-#   alpha=alpha, rho=rho, sigma=sigma
-# )
+(g <- ggplot(ss, aes(dist,prob, group=sim))  + 
+  geom_line(alpha=.4) +
+  expand_limits(y=0:1) + 
+  ylim( 0, min(10, max(ss$prob)) )  +
+  labs(title = title)
+)
+
+
+
+
 
 
 ##############
